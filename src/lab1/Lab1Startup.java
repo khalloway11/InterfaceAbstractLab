83996ff61a801bcf220551005fd60d214a447e46
khalloway11 <zacchius@gmail.com>
2014-02-03 21:42:00 -0600

    Added documentation answering required questions in lab 1

--- src/lab1/Lab1Startup.java
+++ src/lab1/Lab1Startup.java
@@ -20,5 +20,23 @@ public class Lab1Startup {
         introJava.setSection(1);
         introJava.setPrerequisites(introProg.getCourseName());
         System.out.println(introJava.getPrerequisites());
+        
+        Course c1 = new IntroToProgrammingCourse("Introduction to Programming", "152-141");
+        /**
+         * The danger about using the Liskov substitution method is that these objects
+        cannot use anything implemented from the subclass. If the majority of the 
+        implementation is present in the abstract class, and any methods or properties
+        in the subclass are not needed in the operation of the program, there are
+        no ill effects. However, if that is ever not the case, then this becomes 
+        * detrimental to the operation of the program.
+        */
     }
 }
+/**
+ * The advantage of using an abstract class  as opposed to an interface is that
+ * any common methods and properties can be implemented in the abstract class
+ * and be inherited to all subclasses easily. It can also hold abstract methods
+ * for implementing a common method that may require unique implementations within
+ * the subclasses. 
+ * 
+ */
